27d6dfa4f982604e10f8a9de060b6fcba7b0b02b
Daniela García <danielagarcia9@gmail.com>
2020-07-30 00:06:54 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -29,9 +29,10 @@ function ternaryTeenager(age) {
 }
 
 function switchAge(age) {
-  if (age >= 13 && age <= 19) {
+  case age >= 13 && age <= 19) :
     return "You are a teenager"
-  } else {
+    break;
+  case
     return "You have an age"
   }
 }
