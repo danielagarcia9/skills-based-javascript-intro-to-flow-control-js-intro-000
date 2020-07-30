a48b70f994ae58b4f88e858c2a5efb42e7b705f3
Daniela García <danielagarcia9@gmail.com>
2020-07-30 00:13:32 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -34,6 +34,16 @@ function switchAge(age) {
       return "You are a teenager"
     case age === 14 :
       return "You are a teenager"
+    case age === 15 :
+      return "You are a teenager"
+    case age === 16 :
+      return "You are a teenag
+    case age === 17 :
+      return "You are a teenager"
+    case age === 18 :
+      return "You are a teenager"
+    case age === 19 :
+      return "You are a teenager"
     default :
     return "You have an age"
   }
